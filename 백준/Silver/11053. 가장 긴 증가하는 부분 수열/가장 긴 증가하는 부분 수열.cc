2a0bbf2c7bf733b65fe32798533acac9656a5c28
dwaknaa <128336371+dwaknaa@users.noreply.github.com>
2024-05-20 11:16:26 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N;
+vector<int> dp;
+vector<int> vec;
+
+int main()
+{
+    cin.tie(0);
+    
+    cin >> N;
+
+    vec.assign(N+1, 0);
+    for(int i=1; i<=N; i++)
+    {
+        cin >> vec[i];
+    }
+
+    dp.assign(N+1, 0);
+    dp[1] = 1;
+    for(int i=2; i<=N; i++)
+    {
+        int min = 0;
+        for(int j=1; j<i; j++)
+        {
+            if(vec[j]<vec[i] && dp[j]>min) min = dp[j];
+        }
+
+        dp[i] = min+1;
+    }
+
+    sort(dp.begin(), dp.end());
+
+    cout << dp.back() << endl;
+
+    return 0;
+}
